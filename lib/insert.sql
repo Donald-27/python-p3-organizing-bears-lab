f5f881928d4c555d0cdb3172d54f3cab83d57435
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-19 14:21:06 +0300

    Add SQL queries to retrieve bear data for select tests

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+('Mr. Chocolate', 5, 'M', 'brown', 'calm', 1),
+('Rowdy', 8, 'M', 'black', 'aggressive', 1),
+('Tabitha', 4, 'F', 'white', 'playful', 1),
+('Sergeant Brown', 7, 'M', 'dark brown', 'serious', 1),
+('Melissa', 6, 'F', 'grey', 'friendly', 1),
+('Grinch', 9, 'M', 'green', 'grumpy', 0),
+('Wendy', 3, 'F', 'golden', 'gentle', 1),
+(NULL, 2, 'M', 'black', 'curious', 1);  -- unnamed bear
